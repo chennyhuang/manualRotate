b45e074d6cd06b006388de42c25dde97b3b038cc
chennyhuang <huangzhenyu!5>
2018-07-29 09:54:27 +0800

    Initial Commit

---
+++ manualRotate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  manualRotate
+//
+//  Created by hzy on 2018/7/29.
+//  Copyright © 2018年 hzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ manualRotate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  manualRotate
+//
+//  Created by hzy on 2018/7/29.
+//  Copyright © 2018年 hzy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
